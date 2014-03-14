16991523a8428c52cff8956dec32277725815fcd
Matt Knicos <mknicos@gmail.com>
2014-03-14 13:29:58 -0500

    Facebook login works! :metal:

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var d = require('../lib/request-debug');
+var passport = require('passport');
 var initialized = false;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function(req, res, next){
   if(!initialized){
@@ -13,6 +15,26 @@ module.exports = function(req, res, next){
 };
 
 function load(app, fn){
+  passport.serializeUser(function(user, done){
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(obj, done){
+    done(null, obj);
+  });
+
+  passport.use(new FacebookStrategy({
+      clientID: '1430897753818675',
+      clientSecret: 'a1a805afc58ab0421b780187acd29a66',
+      callbackURL: 'http://192.168.11.193:4000/auth/facebook/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      console.log('>>>>>>>>>>>>>>>>', profile);
+        console.log('>>>>>>>>>>>>>>>>', accessToken);
+        done(null, profile);
+    }
+  ));
+
   var home = require('../routes/home');
   var users = require('../routes/users');
   var listings = require('../routes/listings');
@@ -26,6 +48,10 @@ function load(app, fn){
   app.get('/listings/new', d, listings.fresh);
   app.get('/listings', d, listings.index);
   app.get('/listings/query', d, listings.query);
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+  app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/listings',
+                                        failureRedirect: '/login' }));
   console.log('Routes Loaded');
   fn();
 }
